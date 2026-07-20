91ecd195fdff6c65a2e32b202a7b9476b213ae5c
Pinox64 <117751506+Pinox64@users.noreply.github.com>
2026-07-20 12:00:53 -0400

    Description position

--- src/pdf/fieldCoordinates.ts
+++ src/pdf/fieldCoordinates.ts
@@ -10,7 +10,7 @@ export const fieldCoordinates={
  purchaseFundraising:{x:220,y:483,size:10},purchaseRevenue:{x:220,y:464,size:10},purchaseNoRevenue:{x:220,y:445,size:10},purchaseOther:{x:220,y:425,size:10},otherDate:{x:422,y:421,width:125,fontSize:9,align:'center' as const},
  equipment:{x:220,y:389,size:10},computerEquipment:{x:220,y:370,size:10},itemOver2000:{x:220,y:389,size:10},itemOver1500:{x:220,y:370,size:10},
  equipmentLife2:{x:475,y:389,size:10},equipmentLife3:{x:505,y:389,size:10},equipmentLife5:{x:532,y:389,size:10},computerLife2:{x:475,y:370,size:10},computerLife3:{x:505,y:370,size:10},computerLife5:{x:532,y:370,size:10},
- project:{x:212.7,y:334.4,width:181,fontSize:10,minFontSize:7},description:{x:214,y:265,width:333,height:66,lines:3,fontSize:10,minFontSize:7},
+ project:{x:212.7,y:334.4,width:181,fontSize:10,minFontSize:7},description:{x:214,y:263,width:333,height:68,lines:3,fontSize:10,minFontSize:7},
  president:{x:214,y:227,width:144,fontSize:9,minFontSize:7},treasurer:{x:395,y:227,width:152,fontSize:9,minFontSize:7},
  presidentSignature:{x:214,y:188,width:144,height:30,fontSize:9},treasurerSignature:{x:395,y:188,width:152,height:30,fontSize:9},
  submittedDate:{x:214,y:151,width:99,fontSize:9,align:'center' as const}
